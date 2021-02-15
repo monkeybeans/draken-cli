11b821a444b4a28e9c536d9823d1b89c5620d3a3
Herr Fisk <mac@noreply.com>
2021-02-15 22:52:23 +0100

    using print method for movies

--- lib/draken-cli.rb
+++ lib/draken-cli.rb
@@ -15,6 +15,14 @@ OptionParser.new do |opts|
     options[:search] = o
   end
 
+  opts.on("--search-title", "Perform search") do |o|
+    options[:title] = o
+  end
+
+  opts.on("--search-director", "Perform search") do |o|
+    options[:directors] = o
+  end
+
   opts.on("-r", "--random", "Random film for you") do |o|
     options[:random] = o
   end
@@ -25,6 +33,21 @@ DRAKEN_HOST="https://www.drakenfilm.se/"
 api = Net::HTTP.new('cms.drakenfilm.se', 443)
 api.use_ssl = true
 
+def print_movie(movie, verbose)
+    puts "# #{movie['title']}"
+    if verbose
+        puts "\n#{movie['synopsis']}\n\n" unless movie['synopsis'].nil?
+        puts "directors: #{movie['directors'].join(', ')}" unless movie['directors'].nil?
+        puts "cast:      #{movie['cast'].join(', ')}" unless movie['cast'].nil?
+        puts "genres:    #{movie['genres'].join(', ')}" unless movie['genres'].nil?
+        puts "country:   #{movie['countriesOfOrigin'].join(', ')}" unless movie['countriesOfOrigin'].nil?
+        puts "length:    #{movie['duration']/60}min" unless movie['duration'].nil?
+        puts
+    end
+    puts "#{DRAKEN_HOST}film/#{movie['slug']}"
+end
+
+#ttps://cms.drakenfilm.se/movies/count?_limit=42&_start=0&_sort=title:ASC&directors_contains=charlie chaplin
 if options[:search]
     if ARGV.length == 0
         puts "Missing search query"
@@ -35,7 +58,8 @@ if options[:search]
         "_limit=100",
         "_start=0",
         "_sort=title:ASC",
-        "title_contains=#{URI.escape(search)}"
+        #"title_contains=#{URI.escape(search)}"
+        "directors_contains=#{URI.escape(search)}"
     ].join('&')
 
     res = api.get("/movies?#{query}")
@@ -43,15 +67,8 @@ if options[:search]
     if res.is_a?(Net::HTTPSuccess)
         movies = JSON.parse(res.body)
         movies.each do |movie|
-            puts "# #{movie['title']}"
-            if options[:verbose]
-                puts movie['synopsis']
-                puts "#{DRAKEN_HOST}film/#{movie['slug']}"
-                puts
-            end
+            print_movie(movie, options[:verbose])
         end
-
-        puts "#{}"
     else
         puts "[err] msg: #{res}"
     end
@@ -74,17 +91,7 @@ if options[:random]
         rnd_movie_indx = rand(movies.length)
         movie = movies[rnd_movie_indx]
 
-        puts "# #{movie['title']}"
-        if options[:verbose]
-            puts "\n#{movie['synopsis']}\n\n" unless movie['synopsis'].nil?
-            puts "directors: #{movie['directors'].join(', ')}" unless movie['directors'].nil?
-            puts "cast:      #{movie['cast'].join(', ')}" unless movie['cast'].nil?
-            puts "genres:    #{movie['genres'].join(', ')}" unless movie['genres'].nil?
-            puts "country:   #{movie['countriesOfOrigin'].join(', ')}" unless movie['countriesOfOrigin'].nil?
-            puts "length:    #{movie['duration']/60}min" unless movie['duration'].nil?
-            puts
-        end
-        puts "#{DRAKEN_HOST}film/#{movie['slug']}?#{rnd_letter}-#{rnd_movie_indx}"
+        print_movie(movie, options[:verbose])
     else
         puts "[err] msg: #{res}"
     end
